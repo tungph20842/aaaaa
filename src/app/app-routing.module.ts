85c70858de4ad59806b70174a1c8d3872535c65e
tungak2003 <dasuabest@gmail.com>
2023-06-04 23:41:30 +0700

    aaaa

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+import { ProductListPageComponent } from './admin/product-list-page/product-list-page.component';
+import { NewProductPageComponent } from './admin/new-product-page/new-product-page.component';
+import { UpdateProductPageComponent } from './admin/update-product-page/update-product-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+
+  {
+    path: 'admin', component: LayoutAdminComponent, children: [
+      { path: '', component: ProductListPageComponent },
+      { path: 'new-product', component: NewProductPageComponent },
+      { path: ':id/edit', component: UpdateProductPageComponent },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProductListPageComponent } from './admin/product-list-page/product-list-page.component';
+import { NewProductPageComponent } from './admin/new-product-page/new-product-page.component';
+import { UpdateProductPageComponent } from './admin/update-product-page/update-product-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    LayoutAdminComponent,
+    PageNotFoundComponent,
+    ProductListPageComponent,
+    NewProductPageComponent,
+    UpdateProductPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
